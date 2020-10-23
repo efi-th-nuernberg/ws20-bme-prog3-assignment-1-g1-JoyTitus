7f447770fd0d923cf042e564393f22005593d49b
repl.it user <replituser@example.com>
2020-10-23 10:36:08 +0000

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello joy!");
+    int numberOne = 0;
+    String filter = " ";
+    String filter2 = " ";
+    String filter3 = " ";
+    for (int i=0; i<=200; i++) {
+    System.out.println(numberOne = i);
+    filter = divideByFive(numberOne);
+    filter2 = numberEndsWithNine(numberOne);
+    int numberTwo = i-1;
+    filter3 = twoNumbersDivideByThree(numberOne, numberTwo);
+    numberTwo++;
+    System.out.println(filter + " " +  filter2+ " " + filter3);
+    }
   }
+  public static String divideByFive(int number) {
+    String result = "";
+    if(number%5==0){
+      result = "Number is divisble by 5";
+    }
+    return(result);
+  }
+
+  public static String numberEndsWithNine(int number) {
+    String result = "";
+    if((number%10)==9){
+      result="Number ends with 9";
+    }
+    return(result);
+  }
+
+  public static String twoNumbersDivideByThree(int numberOne, int numberTwo){
+    String result = "";
+    if(((numberOne + numberTwo)%3)==0){
+      result ="Number and predecessor add up to a result and result is divisble by 3";
+    }
+    return(result);
+}
 }
---
+++ Zahlenfilter.java
@@ -0,0 +1,19 @@
+public class Zahlenfilter {
+  public String divideByFive(int number){
+    if(number&5==0){
+      return("durch 5 teilbar");
+
+    }
+  }
+  public String numberEndsWithNine(int number)
+  if((number%10)==9){
+    return("endet auf 9");
+
+  }
+  }
+  public String twoNumbersDivideByThree(int numberOne, int numberTwo){
+    if((numberOne + numberTwo)/3)==0){
+      return("Zahl und Vorgänger addiert ergeben Ergebnis und Ergebnis ist durch 3 teilbar");
+    }
+  }
+}
